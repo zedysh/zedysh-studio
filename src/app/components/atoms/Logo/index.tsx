b085a36c829aac9df5a3a96e77a3e362e10941e9
d3metura <turabelidze.demetre@gmail.com>
2025-08-24 23:24:42 +0400

    setup

---
+++ src/app/components/atoms/Logo/index.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+const ratio = 2.5;
+const width = 250;
+const height = width / ratio;
+
+export default function Logo() {
+  return <Image src="/logo.svg" alt="Zedysh Studio logo" width={width} height={height} />;
+}
