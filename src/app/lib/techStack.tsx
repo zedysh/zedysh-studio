3344e406381bf9e7bad5651e35fad7d146d37457
d3metura <turabelidze.demetre@gmail.com>
2025-10-12 21:31:25 +0400

    fix bug

--- src/app/lib/techStack.tsx
+++ src/app/lib/techStack.tsx
@@ -1,111 +1,83 @@
-import StackIcon from "tech-stack-icons";
-
 const techStack = [
   {
     title: "React",
-    icon: <StackIcon name="react" />,
   },
   {
     title: "Three.js",
-    icon: <StackIcon name="threejs" />,
   },
   {
     title: "Blender",
-    icon: <img src="/icons/blender.svg" alt="Blender" />,
   },
   {
     title: "Unity",
-    icon: <img src="/icons/unity.svg" alt="Unity" />,
   },
   {
     title: "TypeScript",
-    icon: <StackIcon name="typescript" />,
   },
   {
     title: "Next.js",
-    icon: <StackIcon name="nextjs2" />,
   },
   {
     title: "Figma",
-    icon: <StackIcon name="figma" />,
   },
   {
     title: "AWS",
-    icon: <StackIcon name="aws" />,
   },
   {
     title: "Photoshop",
-    icon: <StackIcon name="ps" />,
   },
   {
     title: "Illustrator",
-    icon: <StackIcon name="ai" />,
   },
   {
     title: "Firebase",
-    icon: <StackIcon name="firebase" />,
   },
   {
     title: "MongoDB",
-    icon: <StackIcon name="mongodb" />,
   },
-
+  ,
   {
     title: "HTML",
-    icon: <StackIcon name="html5" />,
   },
   {
     title: "CSS",
-    icon: <StackIcon name="css3" />,
   },
   {
     title: "SCSS",
-    icon: <StackIcon name="sass" />,
   },
   {
     title: "Tailwind CSS",
-    icon: <StackIcon name="tailwindcss" />,
   },
   {
     title: "Vue.js",
-    icon: <StackIcon name="vuejs" />,
   },
   {
     title: "Laravel",
-    icon: <StackIcon name="laravel" />,
   },
   {
     title: "Express",
-    icon: <StackIcon name="expressjs" />,
   },
   {
     title: "Node.js",
-    icon: <StackIcon name="nodejs" />,
   },
   {
     title: "MySQL",
-    icon: <StackIcon name="mysql" />,
   },
   {
     title: "Vercel",
-    icon: <StackIcon name="vercel" />,
   },
   {
     title: "Git",
-    icon: <StackIcon name="git" />,
   },
   {
     title: "Jira",
-    icon: <StackIcon name="jira" />,
   },
   {
     title: "Postman",
-    icon: <StackIcon name="postman" />,
   },
   // add shopify
   {
     title: "Shopify",
-    icon: <StackIcon name="shopify" />,
   },
 ];
 
