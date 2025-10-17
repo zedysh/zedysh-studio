839b2fefbbc2f8c4bfaeb23c05233d5bad541784
d3metura <turabelidze.demetre@gmail.com>
2025-10-17 14:38:07 +0400

    ...

--- src/app/lib/socials.ts
+++ src/app/lib/socials.ts
@@ -1,9 +1,9 @@
 import { ISocialLinks } from "./types";
 
 export const socialLinks: ISocialLinks[] = [
+  { type: "instagram", link: "https://www.instagram.com/zedysh/" },
   { type: "behance", link: "https://www.behance.net/zedysh" },
   { type: "dribbble", link: "https://www.dribbble.com/zedyshstudio" },
-  { type: "instagram", link: "https://www.instagram.com/zedysh/" },
   {
     type: "linkedin",
     link: "https://www.linkedin.com/company/zedyshstudio/",
