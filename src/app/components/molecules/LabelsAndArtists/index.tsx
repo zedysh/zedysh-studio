7fbdbea28707a25604151b54c2aacad6f4aa18f7
d3metura <turabelidze.demetre@gmail.com>
2025-11-26 00:33:44 +0400

    labels to marquee

--- src/app/components/molecules/LabelsAndArtists/index.tsx
+++ src/app/components/molecules/LabelsAndArtists/index.tsx
@@ -2,17 +2,18 @@ import style from "./LabelsAndArtists.module.scss";
 import { musicLabels } from "@/app/lib/labels";
 import { artistsList } from "@/app/lib/artistsList";
 import Title from "../../atoms/Title";
+import Marquee from "react-fast-marquee";
 
 export default function Labels() {
   return (
     <div className={style.container}>
       <Title text="Clients" theme="dark" />
 
-      <div className={style.labels}>
+      <Marquee gradient={false} speed={100} className={style.marquee}>
         {musicLabels.map((label, i) => (
           <img src={label.image} alt={label.title} className={style.image} key={i} />
         ))}
-      </div>
+      </Marquee>
 
       <Title text="Artists" theme="dark" />
 
